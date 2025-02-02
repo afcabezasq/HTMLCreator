9cb9613534846b0628c02818e50178802a1f03dc
Andres Felipe Cabezas Quicano <afcabezasq@unal.edu.co>
2025-02-01 21:43:48 -0500

    Coverter with ui

---
+++ converter.py
@@ -0,0 +1,107 @@
+import csv
+import os
+from datetime import datetime, timedelta
+
+def create_folder(foldername):
+    os.makedirs(foldername, exist_ok=True)
+
+def read_csv(filename, date_format="%Y-%m-%d %H:%M:%S"):
+    with open(filename) as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        groups = {}
+        counter = 0
+        for row in reader:
+    
+            if counter == 0:
+                counter =+ 1
+                continue 
+            if row[3] not in groups:
+                groups[row[3]] = {
+                    'min_date': datetime.strptime(str(row[0]),date_format),
+                    'max_date': datetime.strptime(str(row[0]),date_format),
+                    'entries':[row]
+                    }
+            else:
+                groups[row[3]]['entries'].append(row)
+                current =  datetime.strptime(str(row[0]),date_format)
+                if current < groups[row[3]]['min_date']:
+                    groups[row[3]]['min_date'] = current
+                if current > groups[row[3]]['max_date']:
+                    groups[row[3]]['max_date'] = current
+                    
+    return groups
+
+def generate_html(groups, date_format="%Y-%m-%d %H:%M:%S", output_folder="message_html"):
+    for g in groups:
+        groups[g]['entries'] = sorted(groups[g]['entries'], key=lambda x: datetime.strptime(x[0],date_format))
+        
+        start = datetime.strptime(groups[g]['entries'][0][0],date_format)
+        start = start - timedelta(hours=start.hour)
+        
+        end = datetime.strptime(groups[g]['entries'][-1][0],date_format)
+        end = end - timedelta(hours=end.hour)
+        end_date = end + timedelta(days=1)
+        index = 0
+        current_date = start
+        while index<len(groups[g]['entries']) and current_date < end_date:
+            next_date = current_date + timedelta(days=1)
+            entries_by_day = []
+            while (index<len(groups[g]['entries']) and datetime.strptime(groups[g]['entries'][index][0],date_format) >= current_date 
+                   and datetime.strptime(groups[g]['entries'][index][0], date_format) < next_date
+                   ):
+                entries_by_day.append(groups[g]['entries'][index])
+                index += 1
+            
+            html_content = f"""
+                <html>
+                <head>
+                    <title>{g} - Messages from {current_date.strftime('%Y-%m-%d')}</title>
+                    <style>
+                        body {{ font-family: Arial, sans-serif; background: #f5f5f5; padding: 20px; }}
+                        .chat-container {{ max-width: 500px; margin: auto; background: white; padding: 20px; border-radius: 10px; box-shadow: 0 0 10px rgba(0,0,0,0.1); }}
+                        .message {{ display: flex; flex-direction: column; margin-bottom: 15px; }}
+                        .message-header {{ font-size: 12px; color: #666; margin-bottom: 5px; }}
+                        .message-bubble {{ max-width: 70%; padding: 10px; border-radius: 15px; word-wrap: break-word; }}
+                        .sender {{ align-items: flex-end; }}
+                        .receiver {{ align-items: flex-start; }}
+                        .sender .message-bubble {{ background: #AEDFF7; }}  /* Light Blue */
+                        .receiver .message-bubble {{ background: #FDCB9E; }} /* Light Orange */
+                        .timestamp {{ font-size: 12px; color: #888; text-align: right; margin-top: 5px; }}
+                    </style>
+                </head>
+                <body>
+                    <div class="chat-container">
+                        <h2 style="text-align:center;">{g} - Messages from {current_date.strftime('%Y-%m-%d')}</h2>
+                """
+            sender_receiver = {}
+            for date, sender, receiver, chatgroup, text in entries_by_day:
+                if "sender" not in sender_receiver:
+                    sender_receiver['sender'] = sender
+                    sender_receiver['receiver'] = receiver
+                alignment = "sender" if sender_receiver['sender'] == sender else "receiver"
+                html_content += f"""
+                    <div class="message {alignment}">
+                        <div class="message-header">{sender} - {date}</div>
+                        <div class="message-bubble receiver">
+                            <p>{text}</p>
+                        </div>
+                    </div>
+                    """
+            html_content += "</div></body></html>"
+            safe_group_name = g.replace(" ","")
+            file_name = f"{output_folder}/{safe_group_name}_{current_date.strftime('%Y-%m-%d')}.html"
+            with open(file_name, "w", encoding="utf-8") as f:
+                f.write(html_content)    
+            print(f"Saved: {file_name}")
+                     
+            current_date = next_date
+        
+
+        
+def main():
+    g = read_csv("data/chat_conversation_multi_day.csv")
+    generate_html(g)
+
+
+if __name__ == "__main__":
+    main()
---
+++ converterUi.py
@@ -0,0 +1,66 @@
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import ttk
+from converter import generate_html, read_csv
+
+def browseFiles():
+    filename = filedialog.askopenfilename(initialdir="./",title="Select File", filetypes=(("Text files",
+                                                        "*.csv*"),
+                                                       ("all files",
+                                                        "*.*")))
+    
+    g = read_csv(filename,date_format=selected_format.get())
+    generate_html(g,date_format=selected_format.get())
+    label_file_explorer.configure(text="HTML Files Generated")
+    
+    
+window = tk.Tk()
+# Set window title
+window.title('File Explorer')
+  
+# Set window size
+window.geometry("300x150")
+  
+#Set window background color
+window.config(background = "white")
+  
+# Create a File Explorer label
+label_file_explorer = tk.Label(window, 
+                            text = "File Explorer using Tkinter",
+                            width = 20, height = 4, 
+                            fg = "blue")
+label_select_format = tk.Label(window, 
+                            text = "Select Date Format",
+                            width = 20, height = 4, 
+                            fg = "blue")
+      
+button_explore = tk.Button(window, 
+                        text = "Browse Files",
+                        command = browseFiles) 
+  
+button_exit = tk.Button(window, 
+                     text = "Exit",
+                     command = exit) 
+  
+# Grid method is chosen for placing
+# the widgets at respective positions 
+# in a table like structure by
+# specifying rows and columns
+label_file_explorer.grid(column = 1, row = 1)
+  
+button_explore.grid(column = 1, row = 2)
+label_select_format.grid(column = 0, row = 1)
+  
+
+selected_format = tk.StringVar()
+
+formats =("%Y-%m-%d %H:%M:%S","%Y/%m/%d %H:%M:%S")
+
+row = 2
+for f in formats:
+    r = ttk.Radiobutton(window,text=f,value=f, variable=selected_format)
+    r.grid(column=0,row=row)
+    row += 1
+  
+# Let the window wait for any events
+window.mainloop()
